4b34546253ac7a3884e659702036dcae896961d9
olivier3481 <olivier3481@yahoo.fr>
2020-10-18 13:10:00 +0200

    init

---
+++ JS_Fundamentals/main.js
@@ -0,0 +1,9 @@
+import {someVar, someFunc} from './lib/module.js';
+
+let add = (x, y) => x + y;
+console.log(add(5,3));
+
+// "Some data for output"
+console.log(someVar + someFunc());
+
+const CONST = 10;
